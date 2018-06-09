0955019830eaced09b610768b5cf5c37183f2bcc
Francis27888 <36159833+Francis27888@users.noreply.github.com>
2018-06-09 18:05:28 +0200

    using 2 inditantion

--- app/helpers/houses_helper.rb
+++ app/helpers/houses_helper.rb
@@ -1,9 +1,9 @@
 module HousesHelper
-    def choose_new_or_edit
-        if action_name == 'new' || action_name == 'confirm'
-            confirm_houses_path
+  def choose_new_or_edit
+    if action_name == 'new' || action_name == 'confirm'
+      confirm_houses_path
         elsif action_name == 'edit'
-            house_path
+          house_path
         end
     end
 end
